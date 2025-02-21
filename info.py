e9ebd7684eb1d053efa7d698d320b2368543a71b
loveP87 <lovedpatel888@gmail.com>
2025-02-21 17:51:17 +0530

    Update info.py

--- info.py
+++ info.py
@@ -88,7 +88,7 @@ PUBLIC_FILE_CHANNEL = environ.get('PUBLIC_FILE_CHANNEL', '') # Public Channel Us
 
 
 # Links
-GRP_LNK = environ.get('GRP_LNK', 't.me/afdawuyfuyduygauygw')
+GRP_LNK = environ.get('GRP_LNK', 't.me/+fjVY9T__hx0wMmY9')
 CHNL_LNK = environ.get('CHNL_LNK', 't.me/uygfuwgfugugfg')
 SUPPORT_CHAT = environ.get('SUPPORT_CHAT', 't.me/+K90TwSvypmAyZGY1') # Support Chat Link Without https:// or @
 OWNER_LNK = environ.get('OWNER_LNK', '')
